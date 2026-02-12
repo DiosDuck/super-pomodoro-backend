3cf0c93b83861b667f5c449532a1024f2a288575
DiosDuck <tapucovidiu@gmail.com>
2026-02-12 13:53:55 +0200

    create application tests on profile controller

--- tests/Authentication/Controller/ProfileControllerTest.php
+++ tests/Authentication/Controller/ProfileControllerTest.php
@@ -2,17 +2,98 @@
 
 declare(strict_types=1);
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Authentication\Entity\User;
+use App\Tests\Utils\Controller\CleanWebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProfileControllerTest extends WebTestCase
+class ProfileControllerTest extends CleanWebTestCase
 {
-    public function getProfileUserNotFound(): void
+    public function testGetProfileUserNotFound(): void
     {
-        $client = static::createClient();
-
-        $client->request('GET', '/api/profile');
+        $this->client->request('GET', '/api/profile');
 
         $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testGetProfileUserFound(): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUsername('username');
+        $user->setRoles(['ROLE_USER']);
+        $user->setActivatedAt(new DateTimeImmutable());
+        $user->setDisplayName('Test Me');
+        $user->setIsActive(true);
+        $user->setPassword('password');
+
+        $user = $this->saveUser($user);
+
+        $token = $this->testAndGetLoginToken('username', 'password');
+
+        $this->client->request(
+            'GET',
+            '/api/profile',
+            server: ['HTTP_AUTHORIZATION' => sprintf('Bearer %s', $token)]
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals($user->getDisplayName(), $this->testAndGetJsonResponse('displayName'));
+    }
+
+    public function testDeleteAccountNotLoggedIn(): void
+    {
+        $this->client->request('DELETE', '/api/profile');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testDeleteAccountWrongPassword(): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUsername('username');
+        $user->setRoles(['ROLE_USER']);
+        $user->setActivatedAt(new DateTimeImmutable());
+        $user->setDisplayName('Test Me');
+        $user->setIsActive(true);
+        $user->setPassword('password');
+
+        $user = $this->saveUser($user);
+
+        $token = $this->testAndGetLoginToken('username', 'password');
+
+        $this->client->request(
+            'DELETE',
+            '/api/profile',
+            server: ['HTTP_AUTHORIZATION' => sprintf('Bearer %s', $token)],
+            content: json_encode(['password' => 'wrong_password'], JSON_THROW_ON_ERROR)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    public function testDeleteAccountSuccess(): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUsername('username');
+        $user->setRoles(['ROLE_USER']);
+        $user->setActivatedAt(new DateTimeImmutable());
+        $user->setDisplayName('Test Me');
+        $user->setIsActive(true);
+        $user->setPassword('password');
+
+        $user = $this->saveUser($user);
+
+        $token = $this->testAndGetLoginToken('username', 'password');
+
+        $this->client->request(
+            'DELETE',
+            '/api/profile',
+            server: ['HTTP_AUTHORIZATION' => sprintf('Bearer %s', $token)],
+            content: json_encode(['password' => 'password'], JSON_THROW_ON_ERROR)
+        );
+
+        $this->assertResponseIsSuccessful();
     }
 }
---
+++ tests/Utils/Controller/CleanWebTestCase.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Utils\Controller;
+
+use App\Authentication\Entity\User;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CleanWebTestCase extends WebTestCase
+{
+    protected KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        self::ensureKernelShutdown();
+
+        $this->client = static::createClient();
+
+        $this->client->enableReboot();
+
+        $this->client->getContainer()->get('cache.app')->clear();
+
+        /** @var EntityManagerInterface $em */
+        $em = $this->client->getContainer()->get('doctrine')->getManager();
+
+        $schemaTool = new SchemaTool($em);
+        $metadata = $em->getMetadataFactory()->getAllMetadata();
+
+        if ($metadata) {
+            $schemaTool->dropSchema($metadata);
+            $schemaTool->createSchema($metadata);
+        }
+
+        $em->clear();
+    }
+
+    public function tearDown(): void
+    {
+        if (isset($this->client)) {
+            $em = $this->client->getContainer()->get('doctrine')->getManager();
+            $em->close();
+        }
+
+        self::ensureKernelShutdown();
+        parent::tearDown();
+    }
+
+    protected function saveUser(User $user): User
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->client->getContainer()->get('doctrine')->getManager();
+
+        $passwordHasher = $this->client->getContainer()->get(UserPasswordHasherInterface::class);
+
+        $user->setPassword(
+            $passwordHasher->hashPassword($user, $user->getPassword()),
+        );
+
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+
+    protected function testAndGetLoginToken(string $user, string $password): string
+    {
+        $this->client->request(
+            'POST',
+            '/api/auth/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: json_encode([
+                'username' => $user,
+                'password' => $password,
+            ], JSON_THROW_ON_ERROR)
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        return $this->testAndGetJsonResponse('token');
+    }
+
+    protected function testAndGetJsonResponse(string $key): mixed
+    {
+        /** @var Response $response */
+        $response = $this->client->getResponse();
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey($key, $data);
+        return $data[$key];
+    }
+}
