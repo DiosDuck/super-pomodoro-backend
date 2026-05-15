b1efd63ed952fd9bf99c431b4a63d54a46b87ff1
DiosDuck <tapucovidiu@gmail.com>
2026-05-15 14:55:47 +0300

    fix tests by removing test keys

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,25 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+$jwtKeyDir = dirname(__DIR__).'/var/jwt-test';
+$jwtPrivate = $jwtKeyDir.'/private.pem';
+$jwtPublic = $jwtKeyDir.'/public.pem';
+
+if (!is_file($jwtPrivate) || !is_file($jwtPublic)) {
+    if (!is_dir($jwtKeyDir)) {
+        mkdir($jwtKeyDir, 0700, true);
+    }
+
+    $resource = openssl_pkey_new([
+        'private_key_bits' => 4096,
+        'private_key_type' => OPENSSL_KEYTYPE_RSA,
+    ]);
+
+    openssl_pkey_export($resource, $privatePem);
+    $publicPem = openssl_pkey_get_details($resource)['key'];
+
+    file_put_contents($jwtPrivate, $privatePem);
+    file_put_contents($jwtPublic, $publicPem);
+    chmod($jwtPrivate, 0600);
+}
